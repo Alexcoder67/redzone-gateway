0c588eda1bac0aa0534aed518411cbb0a5c3c87b
Your Name <alexander450e@gmail.com>
2026-06-24 14:08:20 -0400

    initial commit - django event api

---
+++ events/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    location = models.CharField(max_length=200)
+    date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
