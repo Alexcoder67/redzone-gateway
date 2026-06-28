d31cd8256d2bfd3033ec6ff7ec13afc14cb2036f
Zorro-code1 <alexander450e@gmail.com>
2026-06-28 13:43:38 -0400

    add days_until_event serializer method field

--- events/serializers.py
+++ events/serializers.py
@@ -1,8 +1,14 @@
-
 from rest_framework import serializers
 from .models import Event
 
 class EventSerializer(serializers.ModelSerializer):
+    days_until_event = serializers.SerializerMethodField()
+
     class Meta:
         model = Event
-        fields = '__all__'
+        fields = ['id', 'title', 'description', 'location', 'date', 'created_at', 'days_until_event']
+
+    def get_days_until_event(self, obj):
+        from django.utils import timezone
+        delta = obj.date - timezone.now()
+        return delta.days
